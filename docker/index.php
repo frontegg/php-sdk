da85729c59203d6e3b9decb7f8231fc66191043f
Dima Svetlichniy <dsv1kharkiv@gmail.com>
2020-08-26 15:51:36 +0300

    Docker demo for raw PHP
    * Removed credentials from the code.
    * Updated code.

--- docker/index.php
+++ docker/index.php
@@ -7,17 +7,26 @@ use GuzzleHttp\Psr7\Request;
 use Psr\Http\Message\RequestInterface;
 
 /* Configure Frontegg SDK. */
-$clientId = '6da27373-1572-444f-b3c5-ef702ce65123';
-$apikey = '0cf38799-1dae-488f-8dc8-a09b4c397ad5';
-$tenantId = 'tacajob400@icanav.net';
+/**
+ * Your Client ID. You can setup env variable or change default value here.
+ */
+$clientId = getenv('FRONTEGG_CLIENT_ID')
+    ? getenv('FRONTEGG_CLIENT_ID')
+    : '6da27373-1572-444f-b3c5-ef702ce65123';
+/**
+ * Your Secret API Key. You can setup env variable or change default value here.
+ */
+$apikey = getenv('FRONTEGG_CLIENT_SECRET_KEY')
+    ? getenv('FRONTEGG_CLIENT_SECRET_KEY')
+    : '0cf38799-1dae-488f-8dc8-a09b4c397ad5';
 $config = [
     'clientId' => $clientId,
     'clientSecret' => $apikey,
     'apiBaseUrl' => 'https://dev-api.frontegg.com/',
-    'contextResolver' => function (RequestInterface $request) use ($tenantId) {
+    'contextResolver' => function (RequestInterface $request) {
         return [
-            'tenantId' => $tenantId,
-            'userId' => 'test-user-id',
+            'tenantId' => 'tacajob400@icanav.net',
+            'userId' => 'TEST-USER-ID',
             'permissions' => [],
         ];
     },
@@ -37,7 +46,8 @@ if (isset($_SERVER['REQUEST_URI'])
 
     $response = $frontegg->forward($request);
 
-    $decodedBody = json_decode($response->getBody(), true) ?? $response->getBody();
+    $decodedBody = json_decode($response->getBody(), true) ??
+        $response->getBody();
     print "<pre>";
     var_dump(
         $response->getHttpResponseCode(),
