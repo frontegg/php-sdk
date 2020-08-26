7d5052d971ea286338aef7d14852089448c3d955
Dima Svetlichniy <dsv1kharkiv@gmail.com>
2020-08-26 11:25:43 +0300

    Task "Docker demo for raw PHP"
    * Created demo environment with docker for raw PHP.

---
+++ docker/index.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Frontegg\Frontegg;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Message\RequestInterface;
+
+/* Configure Frontegg SDK. */
+$clientId = '6da27373-1572-444f-b3c5-ef702ce65123';
+$apikey = '0cf38799-1dae-488f-8dc8-a09b4c397ad5';
+$tenantId = 'tacajob400@icanav.net';
+$config = [
+    'clientId' => $clientId,
+    'clientSecret' => $apikey,
+    'apiBaseUrl' => 'https://dev-api.frontegg.com/',
+    'contextResolver' => function (RequestInterface $request) use ($tenantId) {
+        return [
+            'tenantId' => $tenantId,
+            'userId' => 'test-user-id',
+            'permissions' => [],
+        ];
+    },
+    'disableCors' => false,
+];
+
+$frontegg = new Frontegg($config);
+
+/**
+ * Setup routing rule for "/frontegg" URIs.
+ * Can be a part of middleware f.e. in Laravel.
+ */
+if (isset($_SERVER['REQUEST_URI'])
+    && strpos($_SERVER['REQUEST_URI'], '/frontegg') === 0
+) {
+    $request = new Request($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+
+    $response = $frontegg->forward($request);
+
+    $decodedBody = json_decode($response->getBody(), true) ?? $response->getBody();
+    print "<pre>";
+    var_dump(
+        $response->getHttpResponseCode(),
+        $decodedBody,
+        $response->getHeaders()
+    );
+    print "</pre>";
+    exit;
+}
+
+print "<pre>";
+printf('"%s" URL does not match to "%s"', $_SERVER['REQUEST_URI'], '/frontegg');
+print "</pre>";
